092602126853da5d5ab7bb29bf2cc209c9c6bf17
Zach Morrissey <zachmor05@gmail.com>
2023-11-17 09:40:20 -0800

    Update pascals_triangle.py

--- py_leet/pascals_triangle.py
+++ py_leet/pascals_triangle.py
@@ -3,14 +3,17 @@ from typing import List
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        output: List[List[int]] = []
+        if numRows == 0:
+            return []
 
-        for i in range(numRows):
+        output: List[List[int]] = [[1]]
+
+        for i in range(1, numRows):
             row: List[int] = []
-            for j in range(i):
-                left = output[i - 1][j - 1] if j > 0 & i > 0 else 1
-                right = output[i - 1][j] if i > 0 and j < i - 1 else 0
-                row.append(left + right)
+            for j in range(i + 1):
+                left = output[i - 1][j - 1] if j > 0 and i > 0 else 0
+                right = output[i - 1][j] if i > 0 and j < i else 0
+                row.append(max(left + right, 1))
 
             output.append(row)
 
